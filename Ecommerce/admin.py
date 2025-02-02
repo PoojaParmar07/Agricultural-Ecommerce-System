c60b2c173a785fb182198ee42163a3e47afa17ae
pratha_patel <prathapatel1606@gmail.com>
2025-02-02 19:35:26 +0530

    commit

--- Ecommerce/admin.py
+++ Ecommerce/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category,Brand,Product,ProductVariant,ProductBatch,Payment,Order,Order_Item
+from .models import *
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Brand)
@@ -9,3 +9,7 @@ admin.site.register(ProductBatch)
 admin.site.register(Payment)
 admin.site.register(Order)
 admin.site.register(Order_Item)
+admin.site.register(Cart)
+admin.site.register(CartItem)
+admin.site.register(Wishlist)
+admin.site.register(WishlistItem)
