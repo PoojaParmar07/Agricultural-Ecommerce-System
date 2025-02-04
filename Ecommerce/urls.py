194704ad38a8268f56ffc2a56702925261c5847a
pratha_patel <prathapatel1606@gmail.com>
2025-02-04 22:27:23 +0530

    commit

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -7,6 +7,7 @@ app_name = 'Ecommerce'
 
 urlpatterns = [
     path('',home,name='home'),
+    path('product_list/',product_list,name='product_list'),
    
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- Ecommerce/views.py
+++ Ecommerce/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from django.http import Http404
 from django.contrib import messages
-from .models import *
+from django.contrib.auth.decorators import login_required
+from Ecommerce.models import *
 from .forms import *
+from django.db.models import Avg
+
+
 
 def is_admin_user(user):
     return user.is_staff  # or use is_superuser if you're referring to admin access
@@ -13,7 +17,30 @@ def is_admin_user(user):
 def home(request):
     return render(request,'Ecommerce/base.html')
 
+@login_required
 
+
+def product_list(request):
+    products = Product.objects.all()
+    
+    product_data = []
+    for product in products:
+        variant = ProductVariant.objects.filter(product=product).first()  # Get first variant
+        if variant:
+            inventory = Inventory.objects.filter(batch__variant=variant).first()  # Get price from inventory
+            sales_price = inventory.sales_price if inventory else None
+        else:
+            sales_price = None
+        rating=Review.objects.filter(product=product).aggregate(avg_rating=Avg('rating'))['avg_rating']
+
+        product_data.append({
+            'product_name': product.product_name,
+            'product_image': product.product_image.url,
+            'sales_price': sales_price,
+            'rating':rating
+        })
+
+    return render(request, 'Ecommerce/product_list_page.html', {'products': product_data})
 
 
 
