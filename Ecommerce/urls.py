60f7467db3faa047c5eac16a86eec0065b4445e3
pratha_patel <prathapatel1606@gmail.com>
2025-02-08 09:24:07 +0530

    commit

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('',home,name='home'),
 
     path('homebody/',homebody,name='homebody'),
-    path('product_list/', product_list, name='product_list')
+    path('product_list/<int:category_id>/', product_list, name='product_list')
 
    
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
