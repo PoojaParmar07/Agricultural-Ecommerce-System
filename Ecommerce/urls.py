885047a0dac8df1b1c643d0cf9bf259875e4b7b1
poojaparmar <poojaparmar4014@gmail.com>
2025-02-09 23:32:36 +0530

    commit

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -11,9 +11,9 @@ urlpatterns = [
     path('',homebody,name='homebody'),
     path('product_list/<int:category_id>/', product_list, name='product_list'),
     path('product_view/<int:product_id>/',product_view,name='product_view'),
-    # path('cart_view/',cart_view, name='cart_view'),
-    # path('add-to-cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
-    # path('remove-from-cart/<int:item_id>/', remove_from_cart, name='remove_from_cart'),
+    path('cart_view/',cart_view, name='cart_view'),
+    path('add-to-cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
+    path('remove-from-cart/<int:item_id>/', remove_from_cart, name='remove_from_cart'),
 
    
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
